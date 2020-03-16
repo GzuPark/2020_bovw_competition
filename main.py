ff5286c9bbcbd7a7671f7dc4687e03c8fac6f385
Jongmin Park <jmpark@rcv.sejong.ac.kr>
2020-03-16 05:03:29 +0000

    Change search pipeline
    - Add classifier method
    - Add gamma params on SVC
    - Add resize image
    - Apply feature cache files: reduce 35s

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import datetime
+import itertools
 import os
 
 import cv2
@@ -17,11 +18,15 @@ import utils
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--force-train', action='store_true', help='Force training even if a codebook have')
+    parser.add_argument('--image-size', type=int, nargs='+', default=[0, 0], help='Resize image size | (0,0) means that do not resize')
     parser.add_argument('--verbose', type=int, default=0, help='Verbose level of scikit-learn')
     parser.add_argument('--ratio', type=float, default=0.25, help='Split ratio of train / validation')
     parser.add_argument('--seed', type=int, default=42, help='Random seed number')
     parser.add_argument('--voc-size', type=int, default=200, help='Size of vocabulary')
-    parser.add_argument('-C', '--regularization', type=float, default=0.1, help='Regularization parameter in LinearSVC')
+    parser.add_argument('-C', '--regularization', type=float, default=0.1, help='Regularization parameter in LinearSVC/SVC')
+    parser.add_argument('--gamma', type=str, default='scale', help='Gamma parameter in SVC')
+    parser.add_argument('--clf_method', type=str, default='LinearSVC', help='Select classifier method')
+    parser.add_argument('--result-log', type=str, default='parameter_accuracy.log', help='Log file name')
     args = parser.parse_args()
 
     return args
@@ -64,7 +69,7 @@ def get_data(ratio, seed):
 
 
 @utils.timer
-def load_data(data):
+def load_data(data, size):
     X_train = []
     y_train = []
     X_val = []
@@ -74,6 +79,9 @@ def load_data(data):
     # ---------- train ----------
     for img_label, img_path_list in data['train'].items():
         for img_path in img_path_list:
+            img = cv2.imread(img_path, 0)
+            if (size[0] != 0) and (size[1] != 0):
+                img = cv2.resize(img, (size[0], size[1]), interpolation=cv2.INTER_LINEAR)
             X_train.append(cv2.imread(img_path, 0))
             y_train.append(img_label)
 
@@ -111,17 +119,20 @@ def train_clustering(descriptors, voc_size, seed, verbose=0):
 
 @utils.timer
 def represent_histogram(feature, codebook, minlength):
-    train_hist = []
+    hist = []
     for feat in feature:
         pred = codebook.predict(feat)
-        train_hist.append(np.bincount(pred, minlength=minlength).reshape(1, -1).ravel())
+        hist.append(np.bincount(pred, minlength=minlength).reshape(1, -1).ravel())
 
-    return np.array(train_hist)
+    return np.array(hist)
 
 
 @utils.timer
-def predict(hist_train, hist_val, y_train, y_val, seed, C):
-    clf = LinearSVC(random_state=seed, C=C, max_iter=3000).fit(hist_train, y_train)
+def predict(hist_train, hist_val, y_train, y_val, method, seed, C, gamma):
+    if method == 'LinearSVC':
+        clf = LinearSVC(random_state=seed, C=C, max_iter=3000).fit(hist_train, y_train)
+    elif method == 'SVC':
+        clf = SVC(random_state=seed, C=C, gamma=gamma, max_iter=3000).fit(hist_train, y_train)
     pred = clf.predict(hist_val)
     score = accuracy_score(y_val, pred)
 
@@ -131,28 +142,40 @@ def predict(hist_train, hist_val, y_train, y_val, seed, C):
 def run(args):
     # Load and split data
     data, real_path = get_data(args.ratio, args.seed)
-    X_train, y_train, X_val, y_val, X_test = load_data(data)
-
-    # Extract features
-    feature_train = extract_features(X_train)
-    feature_val = extract_features(X_val)
+    X_train, y_train, X_val, y_val, X_test = load_data(data, args.image_size)
     
-    # Generate codebook
     result_path = os.path.join(real_path, 'result')
-
     if not os.path.exists(result_path):
         os.makedirs(result_path)
 
+    # Extract features
+    feat_train_filename = 'feat_train_{}_{}.p'.format(str(args.image_size[0]), str(args.image_size[1]))
+    feat_val_filename = 'feat_val_{}_{}.p'.format(str(args.image_size[0]), str(args.image_size[1]))
+    feat_train_path = os.path.join(result_path, feat_train_filename)
+    feat_val_path = os.path.join(result_path, feat_val_filename)
+
+    if (not os.path.exists(feat_train_path)) or (not os.path.exists(feat_val_path)) or (args.force_train):
+        feature_train = extract_features(X_train)
+        feature_val = extract_features(X_val)
+        utils.safe_pickle_dump(feature_train, feat_train_path)
+        utils.safe_pickle_dump(feature_val, feat_val_path)
+        print('Save caches of features')
+    else:
+        feature_train = utils.pickle_load(feat_train_path)
+        feature_val = utils.pickle_load(feat_val_path)
+        print('Complete to load features')
+    
+    # Generate codebook
     codebook_filename = 'codebook_{}_{}_{}.p'.format(str(args.ratio), str(args.seed), str(args.voc_size))
     codebook_path = os.path.join(result_path, codebook_filename)
 
     if (not os.path.exists(codebook_path)) or (args.force_train):
         codebook = train_clustering(feature_train, args.voc_size, args.seed, args.verbose)
         utils.safe_pickle_dump(codebook, codebook_path)
-        print('[ INFO ] Complete to train')
+        print('Complete to train')
     else:
         codebook = utils.pickle_load(codebook_path)
-        print('[ INFO ] Complete to load \' {} \''.format(codebook_filename))
+        print('Complete to load \' {} \''.format(codebook_filename))
     
     # Represent image using codebook
     hist_train = represent_histogram(feature_train, codebook, args.voc_size)
@@ -163,32 +186,46 @@ def run(args):
     hist_val = scaler.transform(hist_val)
 
     # Predict
-    pred, score = predict(hist_train, hist_val, y_train, y_val, args.seed, args.regularization)
+    pred, score = predict(hist_train, hist_val, y_train, y_val, args.clf_method, args.seed, args.regularization, args.gamma)
     print('[ INFO ] Accuracy: {:.3f} %\n'.format(score * 100))
 
     # Logging
     KST = datetime.timezone(datetime.timedelta(hours=9))
-    now = datetime.datetime.now(tz=KST).strftime('%Y-%m-%d-%H-%M')
+    now = datetime.datetime.now(tz=KST).strftime('%Y-%m-%d-%H-%M-%S')
 
-    log_path = os.path.join(result_path, 'parameter_accuracy.log')
+    log_path = os.path.join(result_path, args.result_log)
     with open(log_path, 'a') as f:
-        f.write('{}\t{:.5f}\t\t{}\n'.format(now, score, args))
+        f.write('{}\t{:.5f}\t{}\n'.format(now, score, args))
 
 
 def main():
     args = get_args()
 
-    voc_size = np.arange(100, 400, 100)
-    regularization = np.arange(0.001, 0.1, 0.001)
+    voc_size = np.arange(50, 401, 50)
+    clf_method = ['LinearSVC', 'SVC']
+    regularization = np.arange(0.0001, 0.0101, 0.0001)
+    gamma = ['scale', 'auto']
 
-    for _voc in voc_size:
-        args.voc_size = _voc
-        args.force_train = True
-        for _reg in regularization:
-            args.regularization = _reg
-            run(args)
+    knob_params = [voc_size, clf_method, regularization, gamma]
+    comb_params = list(itertools.product(*knob_params))
+    total_comb = len(comb_params)
+
+    prev_voc_size = 0
+    print('[ INFO ] Total combination: {}'.format(total_comb))
+
+    for i, params in enumerate(comb_params):
+        if prev_voc_size == params[0]:
             args.force_train = False
-
+        else:
+            prev_voc_size = params[0]
+            args.force_train = True
+        args.voc_size = params[0]
+        args.clf_method = params[1]
+        args.regularization = params[2]
+        args.gamma = params[3]
+        print('Start to train [ {}/{} ]:\n Vocab:\t{}\n Clf:\t{}\n C:\t{}\n Seed:\t{}\n'.format(
+            i+1, total_comb, args.voc_size, args.clf_method, args.regularization, args.seed ))
+        run(args)
 
 
 if __name__ == '__main__':
